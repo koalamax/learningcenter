69141e4bd01cb223f83bc767a9ef542f13deec07
최영재 <koalamax1124@naver.com>
2025-05-18 02:10:28 +0900

    Add files via upload

---
+++ HotelReservation/client/pages/ReservationPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReservationPage = () => {
+  return (
+    <div className="container my-4">
+      <h2>예약 완료</h2>
+      <p>예약이 성공적으로 처리되었습니다. 마이페이지에서 예약내역을 확인하실 수 있습니다.</p>
+    </div>
+  );
+};
+
+export default ReservationPage;
